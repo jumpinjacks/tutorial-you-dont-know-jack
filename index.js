319097a551c145d4c70c54b18ec75425a5bb40ab
jumpinjacks <cgpitt+jumpinjacks@gmail.com>
2016-11-05 11:48:13 +1300

    Added inert + directory listing

--- index.js
+++ index.js
@@ -3,26 +3,38 @@
 const hapi = require("hapi")
 const blankie = require("blankie")
 const scooter = require("scooter")
+const inert = require("inert")
+
 const server = new hapi.Server()
 
 server.connection({
     "port": 3000,
 })
 
-server.route({
-    "method": "get", "path": "/",
-    handler: function (request, reply) {
-        reply("hello world")
-    },
-})
-
-server.register([scooter, {
+server.register([inert, scooter, {
     "register": blankie,
     "options": {
         // ..CSP directives here
         "defaultSrc": "self",
     }
 }], err => {
+
+    server.route({
+        "method": "get", "path": "/",
+        handler: function (request, reply) {
+            reply("hello world")
+        },
+    })
+
+    server.route({
+        "method": "GET", "path": "/{params*}",
+        "handler": {
+            "directory": {
+                "path": "public",
+                "listing": true,
+            },
+        },
+    })
 
     server.start(err => {
         if (err) {
